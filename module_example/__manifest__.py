e8ce473037238da2c70fbbed26e916eece85d0b0
etime-leo <76693269+etime-leo@users.noreply.github.com>
2021-03-26 16:25:19 +0100

    init module

---
+++ module_example/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Module Example',
+    'summary': 'Example of the module',
+    'description': '*',
+    'author': 'me',
+    'website': 'https://etime-leo-odoo-test-develop-2294612.dev.odoo.com/',
+    'category': 'training',
+    'version': '0.1',
+    'depends': ['base'],
+    'date': [
+
+    ],
+    'demo': [
+
+    ],
+
+}
