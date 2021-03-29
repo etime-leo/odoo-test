5899011429350e000532f7d354afc16d05c2d3e8
etime-leo <76693269+etime-leo@users.noreply.github.com>
2021-03-29 12:41:10 +0200

    add field

--- module_example/models/course.py
+++ module_example/models/course.py
@@ -2,7 +2,18 @@
 
 from odoo import models, fields, api
 
-class Course(models.Model):
 
+class Course(models.Model):
     _name = 'test.course'
     _description = 'course info modify'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='this is a very long title')
+    level = fields.Selection(string='level',
+                             selection=[('beginner', 'Beginner'),
+                                        ('intermidiet', 'Intermidiet'),
+                                        ('advanced', 'Advanced')
+                                        ],
+                             copy=False)
+
+    active = fields.Boolean(string='Active', default=True)
