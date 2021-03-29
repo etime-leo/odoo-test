8f65f88264f05f18e893595debfe82775a33d616
etime-leo <76693269+etime-leo@users.noreply.github.com>
2021-03-29 16:07:49 +0200

    orm add spaceship

--- module_example/models/spaceship.py
+++ module_example/models/spaceship.py
@@ -5,14 +5,13 @@ from odoo import models, fields, api
 
 class Spaceship(models.Model):
     _name = 'test.spaceship'
-    _description = 'This is the spaceship of the excercise 1'
+    _description = 'This is the spaceship of the exercise 1'
 
     name = fields.Char(string='name', required=True)
     description = fields.Text(string='this is a very long title')
     dimension = fields.Float('feet', digits=(12,2))
     fuel = fields.Float('gallons', digits=(12,1))
-    n_passenger = fields.Integer()
-    active = fields.Boolean()
+    n_passenger = fields.Integer('passenger')
     ship_type = fields.Selection(string='type',
                                  selection=[('small', 'Sidewinder'),
                                             ('medium', 'Cobra MK IV'),
