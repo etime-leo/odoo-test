2edf41cd8bbe973950022f8c9b7e8a6b10915bca
etime-leo <76693269+etime-leo@users.noreply.github.com>
2021-03-30 15:58:28 +0200

    decorator

--- module_example/models/course.py
+++ module_example/models/course.py
@@ -1,6 +1,7 @@
 # _*_ coding: utf-8 _*_
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 
 class Course(models.Model):
@@ -17,3 +18,21 @@ class Course(models.Model):
                              copy=False)
 
     active = fields.Boolean(string='Active', default=True)
+
+    base_price = fields.Float(string='base price', default=0.00)
+
+    additional_fee = fields.Float(string='add fee', default=10.00)
+
+    total_price = fields.Float(string='Tototal price', readonly=True)
+
+    @api.onchange('base price', 'additional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base price sux')
+        self.total_price = self.base_price + self.additional_fee
+
+    @api.constraint('addition_fee')
+    def _check_addition_fee(self):
+        for record in self:
+            if record.addition_fee < 10.00:
+                raise ValidationError(f'addition fiel sux: {record.addition_fee}')
